2dd6cdcec3fd03603dc292f3c622785865736a91
Alan Suchowolski <asucho3@gmail.com>
2023-07-17 20:13:25 -0300

    CORS

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ app.use((req, res, next) => {
   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
   res.setHeader("Access-Control-Max-Age", 7200);
 
+  console.log(req.headers);
+  console.log(res.headers);
   next();
 });
 
